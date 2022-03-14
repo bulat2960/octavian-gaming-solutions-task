4316e6af7de703eda9dbe521dee6ae926a2f4354
Bulat Gubaydullin <gubaydullin.bulat7@gmail.com>
2022-03-14 21:50:05 +0300

    Исправления в использовании объекта Sprite

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,14 +7,11 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        const background = new Sprite(this, Config.width / 2, Config.height / 2, 'background')
-        const machine = new Sprite(this, Config.width / 2, Config.height / 2, 'machine')
+        const background = this.add.sprite(Config.width / 2, Config.height / 2, 'background')
+        const machine = this.add.sprite(Config.width / 2, Config.height / 2, 'machine')
 
         this.startButton = new Sprite(this, Config.width * 0.9,  Config.height * 0.2, 'play').setScale(0.5)
         this.stopButton = new Sprite(this, Config.width * 0.9, Config.height * 0.8, 'stop').setScale(0.5)
-        
-        this.startButton.setInteractive(true)
-        this.stopButton.setInteractive(true)
 
         this.startButton.on('pointerdown', this.test, this)
     }
